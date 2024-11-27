b2d8cccf71b66546b7295d9f2bd71647123db001
carolinarraposo <carolina.reis.raposo@ubi.pt>
2024-11-27 10:48:37 +0000

    first commit

---
+++ README.md.py
@@ -0,0 +1,8 @@
+Este package ilustra o desenvolvimento de um drawing
+
+##Instalation
+pip install git+https://github.com/carolinarraposo/drawing1.git
+
+##Usage
+from drawing.colors import *
+from drawing.shapes import *
